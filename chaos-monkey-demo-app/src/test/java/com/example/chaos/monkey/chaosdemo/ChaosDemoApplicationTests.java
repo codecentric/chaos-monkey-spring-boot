3deec1fbc0dd8cb5c3b0551aa2bbfb73699e8a1d
Benjamin Wilms <benjamin.wilms@codecentric.de>
2018-04-18 14:46:32 +0200

    Refactoring to multi-module project

---
+++ chaos-monkey-demo-app/src/test/java/com/example/chaos/monkey/chaosdemo/ChaosDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.chaos.monkey.chaosdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChaosDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
