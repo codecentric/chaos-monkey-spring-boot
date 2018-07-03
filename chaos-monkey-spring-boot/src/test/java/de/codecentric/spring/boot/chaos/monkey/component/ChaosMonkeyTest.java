ced6b31f7db7a1aedee5174ca1295f2c0c7d6f7e
deelthor <thorsten.deelmann@codecentric.de>
2018-07-03 09:20:57 +0200

    unit tests for 'All assaults active''

--- chaos-monkey-spring-boot/src/test/java/de/codecentric/spring/boot/chaos/monkey/component/ChaosMonkeyTest.java
+++ chaos-monkey-spring-boot/src/test/java/de/codecentric/spring/boot/chaos/monkey/component/ChaosMonkeyTest.java
@@ -88,6 +88,55 @@ public class ChaosMonkeyTest {
 
     }
 
+    @Test
+    public void allAssaultsActiveExpectLatencyAttack() {
+        given(this.assaultProperties.isExceptionsActive()).willReturn(true);
+        given(this.assaultProperties.isLatencyActive()).willReturn(true);
+        given(this.latencyAssault.isActive()).willReturn(true);
+        given(this.assaultProperties.isKillApplicationActive()).willReturn(true);
+        given(this.assaultProperties.chooseAssault(3)).willReturn(1);
+
+        chaosMonkey.callChaosMonkey();
+
+        verify(latencyAssault, times(1)).attack();
+    }
+
+    @Test
+    public void allAssaultsActiveExpectExceptionLogging() {
+        exception.expect(RuntimeException.class);
+        exception.expectMessage("Chaos Monkey - RuntimeException");
+
+        given(this.assaultProperties.isExceptionsActive()).willReturn(true);
+        given(this.assaultProperties.isLatencyActive()).willReturn(true);
+        given(this.latencyAssault.isActive()).willReturn(true);
+        given(this.assaultProperties.isKillApplicationActive()).willReturn(true);
+        given(this.assaultProperties.chooseAssault(3)).willReturn(2);
+
+        chaosMonkey.callChaosMonkey();
+
+        verify(mockAppender, times(1)).doAppend(captorLoggingEvent.capture());
+
+        assertEquals(Level.INFO, captorLoggingEvent.getValue().getLevel());
+        assertEquals("Chaos Monkey - exception", captorLoggingEvent.getValue().getMessage());
+    }
+
+    @Test
+    public void allAssaultsActiveExpectKillLogging() {
+        given(this.assaultProperties.isExceptionsActive()).willReturn(true);
+        given(this.assaultProperties.isLatencyActive()).willReturn(true);
+        given(this.latencyAssault.isActive()).willReturn(true);
+        given(this.assaultProperties.isKillApplicationActive()).willReturn(true);
+        given(this.assaultProperties.chooseAssault(3)).willReturn(3);
+
+        chaosMonkey.callChaosMonkey();
+
+        verify(mockAppender, times(2)).doAppend(captorLoggingEvent.capture());
+
+        assertEquals(Level.INFO, captorLoggingEvent.getAllValues().get(0).getLevel());
+        assertEquals(Level.INFO, captorLoggingEvent.getAllValues().get(1).getLevel());
+        assertEquals("Chaos Monkey - I am killing your Application!", captorLoggingEvent.getAllValues().get(0).getMessage());
+        assertEquals("Chaos Monkey - Unable to kill the App, I am not the BOSS!", captorLoggingEvent.getAllValues().get(1).getMessage());
+    }
 
     @Test
     public void isKillAppAssaultActive() {
