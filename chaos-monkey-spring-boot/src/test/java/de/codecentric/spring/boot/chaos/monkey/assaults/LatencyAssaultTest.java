31bae8d025fe063d9ee81b7591af580182e29d12
deelthor <thorsten.deelmann@codecentric.de>
2018-07-03 09:01:25 +0200

    Header

--- chaos-monkey-spring-boot/src/test/java/de/codecentric/spring/boot/chaos/monkey/assaults/LatencyAssaultTest.java
+++ chaos-monkey-spring-boot/src/test/java/de/codecentric/spring/boot/chaos/monkey/assaults/LatencyAssaultTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.codecentric.spring.boot.chaos.monkey.assaults;
 
 import org.apache.commons.lang3.RandomUtils;
@@ -9,6 +25,9 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.powermock.api.mockito.PowerMockito.*;
 
+/**
+ * @author Thorsten Deelmann
+ */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({LatencyAssault.class, RandomUtils.class})
 public class LatencyAssaultTest {
