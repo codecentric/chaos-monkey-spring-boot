3a35107439d53a56205ab61c51f3916f04a0253e
wonwoo <aoruqjfu@gmail.com>
2018-09-28 13:09:51 +0900

    remove logger

--- chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/watcher/SpringControllerAspect.java
+++ chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/watcher/SpringControllerAspect.java
@@ -22,8 +22,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Benjamin Wilms
@@ -31,7 +29,6 @@ import org.slf4j.LoggerFactory;
 
 @Aspect
 public class SpringControllerAspect extends ChaosMonkeyBaseAspect{
-    private static final Logger LOGGER = LoggerFactory.getLogger(SpringControllerAspect.class);
 
     private final ChaosMonkey chaosMonkey;
 
