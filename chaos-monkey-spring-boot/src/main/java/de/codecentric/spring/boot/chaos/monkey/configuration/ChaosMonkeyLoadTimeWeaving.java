1bfcdf0bfe5e735ce3eedf06e3a403febba67cd2
Benjamin Wilms <benjamin.wilms@codecentric.de>
2018-10-14 11:31:58 +0200

    first draft of CM4SB as external jar

--- chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/configuration/ChaosMonkeyLoadTimeWeaving.java
+++ chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/configuration/ChaosMonkeyLoadTimeWeaving.java
@@ -3,6 +3,7 @@ package de.codecentric.spring.boot.chaos.monkey.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.context.annotation.LoadTimeWeavingConfiguration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.instrument.classloading.LoadTimeWeaver;
 import org.springframework.instrument.classloading.ReflectiveLoadTimeWeaver;
 
@@ -10,6 +11,7 @@ import org.springframework.instrument.classloading.ReflectiveLoadTimeWeaver;
  * @author Benjamin Wilms
  */
 @Configuration
+@Profile("chaos-monkey")
 @EnableLoadTimeWeaving(aspectjWeaving= EnableLoadTimeWeaving.AspectJWeaving.ENABLED)
 public class ChaosMonkeyLoadTimeWeaving extends LoadTimeWeavingConfiguration {
     @Override
