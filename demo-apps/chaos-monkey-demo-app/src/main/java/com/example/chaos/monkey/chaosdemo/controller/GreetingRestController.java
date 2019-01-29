749223aa47ea23687dcab5eed00062179f8ce7c1
Benjamin Wilms <benjamin.wilms@codecentric.de>
2019-01-29 12:47:35 +0100

    preparing release 2.0.2

--- demo-apps/chaos-monkey-demo-app/src/main/java/com/example/chaos/monkey/chaosdemo/controller/GreetingRestController.java
+++ demo-apps/chaos-monkey-demo-app/src/main/java/com/example/chaos/monkey/chaosdemo/controller/GreetingRestController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * Simple REST Controller example
  * @author Benjamin Wilms
  */
 @RestController
 public class GreetingRestController {
+
     @GetMapping("/rest/hello")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("REST hello!");
