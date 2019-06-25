2d719863b8468dd9b790642166f75cef1c09e2dc
Sequarius <sequarius@gmail.com>
2019-06-25 22:55:34 +0800

    fix runChaos.sh package version

--- demo-apps/chaos-monkey-demo-app-ext-jar/runChaos.sh
+++ demo-apps/chaos-monkey-demo-app-ext-jar/runChaos.sh
@@ -1,1 +1,1 @@
-java -cp ./target/chaos-monkey-demo-app-ext-jar-2.0.1-SNAPSHOT.jar -Dloader.path=../../chaos-monkey-spring-boot/target/chaos-monkey-spring-boot-2.0.1-SNAPSHOT-jar-with-dependencies.jar org.springframework.boot.loader.PropertiesLauncher --spring.profiles.active=chaos-monkey
+java -cp ./target/chaos-monkey-demo-app-ext-jar-2.0.2.jar -Dloader.path=../../chaos-monkey-spring-boot/target/chaos-monkey-spring-boot-2.0.2-jar-with-dependencies.jar org.springframework.boot.loader.PropertiesLauncher --spring.profiles.active=chaos-monkey
