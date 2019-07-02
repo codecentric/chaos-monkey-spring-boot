899979c6230d1fbd5421f92b92a489c81b887987
Benjamin Wilms <benjamin.wilms@codecentric.de>
2019-07-02 10:03:16 +0200

    Add new Spring Boot Version 2.1.5 Add new demo and test case for Spring Boot Web Reactive

---
+++ demo-apps/chaos-monkey-web-reactive-app/src/main/java/com/example/chaos/monkey/chaosdemo/component/HelloComponent.java
@@ -0,0 +1,20 @@
+package com.example.chaos.monkey.chaosdemo.component;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Benjamin Wilms
+ */
+@Component
+public class HelloComponent {
+
+    public Mono<ServerResponse> hello(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+                .body(BodyInserters.fromObject("Hello, from Component!"));
+    }
+}
