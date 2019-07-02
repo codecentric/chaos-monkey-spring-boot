5e34d37422929713ee70a171563405d53c0621a5
Benjamin Wilms <benjamin.wilms@codecentric.de>
2019-07-02 10:02:18 +0200

    Update to Spring Boot Version 2.1.5.RELEASE Working on issue #76 and add new Component test case

---
+++ demo-apps/chaos-monkey-demo-app/src/main/java/com/example/chaos/monkey/chaosdemo/component/HelloComponent.java
@@ -0,0 +1,15 @@
+package com.example.chaos.monkey.chaosdemo.component;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Benjamin Wilms
+ */
+@Component
+public class HelloComponent {
+
+    public String sayHello() {
+
+        return "Hello from Component";
+    }
+}
