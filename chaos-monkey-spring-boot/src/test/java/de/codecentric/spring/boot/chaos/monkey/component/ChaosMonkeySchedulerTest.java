8126ba3b3294f77ab019fe39923de8d102bd61f2
Manuel <manuel.wessner@codecentric.de>
2019-08-08 11:28:13 +0200

    Replace powermockito with mockito

--- chaos-monkey-spring-boot/src/test/java/de/codecentric/spring/boot/chaos/monkey/component/ChaosMonkeySchedulerTest.java
+++ chaos-monkey-spring-boot/src/test/java/de/codecentric/spring/boot/chaos/monkey/component/ChaosMonkeySchedulerTest.java
@@ -1,16 +1,13 @@
 package de.codecentric.spring.boot.chaos.monkey.component;
 
 import de.codecentric.spring.boot.chaos.monkey.configuration.AssaultProperties;
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.scheduling.config.CronTask;
 import org.springframework.scheduling.config.ScheduledTask;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
@@ -18,11 +15,9 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.*;
-import static org.powermock.api.mockito.PowerMockito.mock;
 
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(ScheduledTask.class)
+@RunWith(MockitoJUnitRunner.class)
 public class ChaosMonkeySchedulerTest {
     @Mock
     private ScheduledTaskRegistrar registrar;
@@ -79,7 +74,7 @@ public class ChaosMonkeySchedulerTest {
         String schedule = "*/1 * * * * ?";
         when(config.getRuntimeAssaultCronExpression()).thenReturn(schedule);
         when(registrar.scheduleCronTask(any())).thenAnswer(iom -> {
-            iom.getArgumentAt(0, CronTask.class).getRunnable().run();
+            iom.getArgument(0, CronTask.class).getRunnable().run();
             return null;
         });
 
@@ -87,19 +82,8 @@ public class ChaosMonkeySchedulerTest {
         verify(scope).callChaosMonkey();
     }
 
-    private Matcher<CronTask> hasScheduleLike(String schedule) {
-        return new BaseMatcher<CronTask>() {
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("has schedule like").appendText(schedule);
-            }
-
-            @Override
-            public boolean matches(Object o) {
-                return ((CronTask) o).getExpression().equals(schedule);
-            }
-        };
+    private ArgumentMatcher<CronTask> hasScheduleLike(String schedule) {
+        return cronTask -> cronTask.getExpression().equals(schedule);
     }
 
     @Before
