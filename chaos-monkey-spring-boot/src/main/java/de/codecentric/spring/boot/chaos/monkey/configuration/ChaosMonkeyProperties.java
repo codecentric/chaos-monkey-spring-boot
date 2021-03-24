fbb3a4df1e6f523fd8785aa014e6ea53cefba241
Alex Fincham <Alex_Fincham@intuit.com>
2021-03-23 19:01:59 -0700

    Fix chaos.monkey.enabled prop
    Fix chaos.monkey.enabled prop

--- chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/configuration/ChaosMonkeyProperties.java
+++ chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/configuration/ChaosMonkeyProperties.java
@@ -18,7 +18,6 @@ package de.codecentric.spring.boot.chaos.monkey.configuration;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
@@ -26,6 +25,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "chaos.monkey")
 public class ChaosMonkeyProperties {
 
-  @Value("${enabled:false}")
-  private boolean enabled;
+  private boolean enabled = false;
 }
