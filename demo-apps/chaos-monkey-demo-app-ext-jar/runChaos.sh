a93a47605048d44063d5a42c595094d0519bd403
Manuel <wtfjoke@livenet.ch>
2021-04-22 23:39:33 +0200

    :bug: Fix sometimes wrong spring.factories in external jar (#218)
    No fix found for newer maven assembly plugin versions

--- demo-apps/chaos-monkey-demo-app-ext-jar/runChaos.sh
+++ demo-apps/chaos-monkey-demo-app-ext-jar/runChaos.sh
@@ -1,1 +1,1 @@
-java -cp ./target/chaos-monkey-demo-app-ext-jar-2.0.3-SNAPSHOT.jar -Dloader.path=../../chaos-monkey-spring-boot/target/chaos-monkey-spring-boot-2.0.3-SNAPSHOT-jar-with-dependencies.jar org.springframework.boot.loader.PropertiesLauncher --spring.profiles.active=chaos-monkey
+java -cp ./target/chaos-monkey-demo-app-ext-jar-2.3.10-SNAPSHOT.jar -Dloader.path=../../chaos-monkey-spring-boot/target/chaos-monkey-spring-boot-2.3.10-SNAPSHOT-jar-with-dependencies.jar org.springframework.boot.loader.PropertiesLauncher --spring.profiles.active=chaos-monkey
