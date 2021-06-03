983ee609c916fe5b54147ceb32d9581b2cb526d5
Manuel <WtfJoke@users.noreply.github.com>
2021-06-03 14:11:42 +0200

    Fix typo

--- chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/configuration/toggles/ChaosToggleNameMapper.java
+++ chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/configuration/toggles/ChaosToggleNameMapper.java
@@ -4,7 +4,7 @@ import de.codecentric.spring.boot.chaos.monkey.component.ChaosTarget;
 
 /**
  * A way to map individual ChaosTargets (controllers, repositories, etc) and the corresponding
- * method. Implementations will can make the name to toggle mapping as coarse or as detailed as
+ * method. Implementations can make the name to toggle mapping as coarse or as detailed as
  * desired.
  *
  * @author Clint Checketts
