2af026837990c59de6e789e59870591b3e21a39b
Manuel <wtfjoke@livenet.ch>
2022-10-15 22:08:42 +0200

    Remove deprecated calls

--- demo-apps/chaos-monkey-demo-app-unleash-toggles/src/main/java/com/example/chaos/monkey/toggledemo/WebSecurityConfig.java
+++ demo-apps/chaos-monkey-demo-app-unleash-toggles/src/main/java/com/example/chaos/monkey/toggledemo/WebSecurityConfig.java
@@ -19,16 +19,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     private final PasswordEncoder passwordEncoder;
 
@@ -36,18 +35,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().httpBasic().and().logout().permitAll();
+        return http.build();
     }
 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
+    public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user = User.withUsername("user").password(passwordEncoder.encode("password")).roles("USER").build();
-
-        UserDetails chaosuser = User.withUsername("chaosuser").password(passwordEncoder.encode("password")).roles("USER").build();
-
-        return new InMemoryUserDetailsManager(user, chaosuser);
+        UserDetails chaosUser = User.withUsername("chaosuser").password(passwordEncoder.encode("password")).roles("USER").build();
+        return new InMemoryUserDetailsManager(user, chaosUser);
     }
 }
