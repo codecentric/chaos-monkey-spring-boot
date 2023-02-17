9938b4a3b5aa8bfc840a6d1384fae7d82b6bbf21
Manuel <manuel.wessner@codecentric.de>
2023-02-17 14:05:34 +0100

    Improve uniqueness of beans provided by Chaos Monkey (see #339)
    To avoid BeanDefinitionOverrideException in users application code, we should aim for unique bean names.
    
    By prefixing the (common) bean names with chaos monkey it should be unique enough.

--- demo-apps/chaos-monkey-demo-app-naked/src/test/java/com/example/chaos/monkey/chaosdemo/ChaosDemoApplicationTests.java
+++ demo-apps/chaos-monkey-demo-app-naked/src/test/java/com/example/chaos/monkey/chaosdemo/ChaosDemoApplicationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2022 the original author or authors.
+ * Copyright 2018-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,6 @@ public class ChaosDemoApplicationTests {
 
     @Test
     public void checkNotMetricsBean() {
-        assertThrows(NoSuchBeanDefinitionException.class, () -> ctx.getBean("metrics"), "No bean named 'metrics' available");
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ctx.getBean("chaosMonkeyMetrics"), "No bean named 'metrics' available");
     }
 }
