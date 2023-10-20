7cbf14f0b703b8888360d1f8356c0e088c10597b
F43nd1r <lukas.morawietz@codecentric.de>
2023-10-20 15:34:49 +0200

    Always register outgoing watchers so they can be enabled later (#402)
    * always register outgoing watchers so they can be enabled later
    
    * Add changelog

--- chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/configuration/ChaosMonkeyRestTemplateConfiguration.java
+++ chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/configuration/ChaosMonkeyRestTemplateConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2022 the original author or authors.
+ * Copyright 2021-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,6 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@ConditionalOnProperty(prefix = "chaos.monkey.watcher", value = "rest-template", havingValue = "true")
 @ConditionalOnClass(value = RestTemplate.class)
 class ChaosMonkeyRestTemplateConfiguration {
 
--- chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/configuration/ChaosMonkeyWebClientConfiguration.java
+++ chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/configuration/ChaosMonkeyWebClientConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2022 the original author or authors.
+ * Copyright 2021-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,6 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@ConditionalOnProperty(prefix = "chaos.monkey.watcher", value = "web-client", havingValue = "true")
 @ConditionalOnClass(value = WebClient.class)
 class ChaosMonkeyWebClientConfiguration {
 
