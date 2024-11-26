6198877b7a2b9f39a9ae9b72c61510e2c1cfb0dc
Jens Kaiser <jens.kaiser@codecentric.de>
2024-11-26 13:42:52 +0100

    user parameter injection

--- chaos-monkey-spring-boot/src/test/java/de/codecentric/spring/boot/chaos/monkey/component/ChaosMonkeySchedulerTest.java
+++ chaos-monkey-spring-boot/src/test/java/de/codecentric/spring/boot/chaos/monkey/component/ChaosMonkeySchedulerTest.java
@@ -15,10 +15,6 @@
  */
 package de.codecentric.spring.boot.chaos.monkey.component;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.*;
-
 import de.codecentric.spring.boot.chaos.monkey.assaults.CpuAssault;
 import de.codecentric.spring.boot.chaos.monkey.assaults.KillAppAssault;
 import de.codecentric.spring.boot.chaos.monkey.assaults.MemoryAssault;
@@ -34,8 +30,18 @@ import org.springframework.scheduling.config.CronTask;
 import org.springframework.scheduling.config.ScheduledTask;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class ChaosMonkeySchedulerTest {
+
+    private static final Runnable EMPTY_RUNNABLE = () -> {
+    };
 
     @Mock
     private ScheduledTaskRegistrar registrar;
@@ -66,22 +72,19 @@ class ChaosMonkeySchedulerTest {
     }
 
     @Test
-    void shouldScheduleATask() {
-        String schedule = "*/1 * * * * ?";
-        ScheduledTask scheduledTask = mock(ScheduledTask.class);
-        when(memoryAssault.getCronExpression(any())).thenReturn(schedule);
+    void shouldScheduleATask(@Mock ScheduledTask scheduledTask) {
+        when(memoryAssault.getCronExpression(any())).thenReturn("*/1 * * * * ?");
         when(registrar.scheduleCronTask(any())).thenReturn(scheduledTask);
 
         new ChaosMonkeyScheduler(registrar, config, Collections.singletonList(memoryAssault));
 
-        verify(registrar).scheduleCronTask(argThat(hasScheduleLike(schedule)));
+        verify(registrar).scheduleCronTask(argThat(hasScheduleLike("*/1 * * * * ?")));
     }
 
     @Test
-    void shouldNotScheduleNewTasksAfterUnrelatedUpdate() {
-        String schedule = "*/1 * * * * ?";
-        ScheduledTask oldTask = mockScheduledTask("memory", schedule);
-        when(memoryAssault.getCronExpression(any())).thenReturn(schedule);
+    void shouldNotScheduleNewTasksAfterUnrelatedUpdate(@Mock(name = "memory") ScheduledTask oldTask) {
+        when(oldTask.getTask()).thenReturn(new CronTask(EMPTY_RUNNABLE, "*/1 * * * * ?"));
+        when(memoryAssault.getCronExpression(any())).thenReturn("*/1 * * * * ?");
         when(registrar.scheduleCronTask(any())).thenReturn(oldTask);
 
         ChaosMonkeyScheduler cms = new ChaosMonkeyScheduler(registrar, config, Collections.singletonList(memoryAssault));
@@ -91,14 +94,13 @@ class ChaosMonkeySchedulerTest {
 
         cms.reloadConfig();
 
-        verify(registrar, never()).scheduleCronTask(argThat(hasScheduleLike(schedule)));
+        verify(registrar, never()).scheduleCronTask(argThat(hasScheduleLike("*/1 * * * * ?")));
     }
 
     @Test
     void shouldTriggerRuntimeScopeRunAttack() {
-        String schedule = "*/1 * * * * ?";
         when(memoryAssault.isActive()).thenReturn(true);
-        when(memoryAssault.getCronExpression(any())).thenReturn(schedule);
+        when(memoryAssault.getCronExpression(any())).thenReturn("*/1 * * * * ?");
         when(registrar.scheduleCronTask(any())).thenAnswer(iom -> {
             iom.getArgument(0, CronTask.class).getRunnable().run();
             return null;
@@ -109,39 +111,30 @@ class ChaosMonkeySchedulerTest {
     }
 
     @Test
-    void shouldRescheduleOnlyChangedTasks() {
-        String memorySchedule = "*/1 * * * * ?";
-        String killAppSchedule = "*/2 * * * * ?";
-        ScheduledTask memoryTask = mockScheduledTask("memory", memorySchedule);
-        ScheduledTask oldTask = mockScheduledTask("killApp", killAppSchedule);
-        ScheduledTask newTask = mock(ScheduledTask.class);
-        when(memoryAssault.getCronExpression(any())).thenReturn(memorySchedule);
-        when(killAppAssault.getCronExpression(any())).thenReturn(killAppSchedule);
-        when(registrar.scheduleCronTask(argThat(hasScheduleLike(memorySchedule)))).thenReturn(memoryTask);
-        when(registrar.scheduleCronTask(argThat(hasScheduleLike(killAppSchedule)))).thenReturn(oldTask, newTask);
+    void shouldRescheduleOnlyChangedTasks(@Mock ScheduledTask newTask,
+                                          @Mock(name = "memory") ScheduledTask memoryTask,
+                                          @Mock(name = "killApp") ScheduledTask oldTask) {
+        when(memoryTask.getTask()).thenReturn(new CronTask(EMPTY_RUNNABLE, "*/1 * * * * ?"));
+        when(oldTask.getTask()).thenReturn(new CronTask(EMPTY_RUNNABLE, "*/2 * * * * ?"));
+        when(memoryAssault.getCronExpression(any())).thenReturn("*/1 * * * * ?");
+        when(killAppAssault.getCronExpression(any())).thenReturn("*/2 * * * * ?");
+        when(registrar.scheduleCronTask(argThat(hasScheduleLike("*/1 * * * * ?")))).thenReturn(memoryTask);
+        when(registrar.scheduleCronTask(argThat(hasScheduleLike("*/2 * * * * ?")))).thenReturn(oldTask, newTask);
 
         ChaosMonkeyScheduler cms = new ChaosMonkeyScheduler(registrar, config, Arrays.asList(memoryAssault, killAppAssault));
-        verify(registrar).scheduleCronTask(argThat(hasScheduleLike(memorySchedule)));
-        verify(registrar).scheduleCronTask(argThat(hasScheduleLike(killAppSchedule)));
+        verify(registrar).scheduleCronTask(argThat(hasScheduleLike("*/1 * * * * ?")));
+        verify(registrar).scheduleCronTask(argThat(hasScheduleLike("*/2 * * * * ?")));
 
         reset(registrar);
-        String killAppSchedule2 = "*/3 * * * * ?";
-        when(killAppAssault.getCronExpression(any())).thenReturn(killAppSchedule2);
+        when(killAppAssault.getCronExpression(any())).thenReturn("*/3 * * * * ?");
 
         cms.reloadConfig();
-        verify(registrar).scheduleCronTask(argThat(hasScheduleLike(killAppSchedule2)));
+        verify(registrar).scheduleCronTask(argThat(hasScheduleLike("*/3 * * * * ?")));
         verify(memoryTask, never()).cancel();
         verify(oldTask).cancel();
     }
 
     private ArgumentMatcher<CronTask> hasScheduleLike(String schedule) {
         return cronTask -> cronTask != null && cronTask.getExpression().equals(schedule);
-    }
-
-    private static ScheduledTask mockScheduledTask(String name, String schedule) {
-        ScheduledTask scheduledTask = mock(ScheduledTask.class, name);
-        when(scheduledTask.getTask()).thenReturn(new CronTask(() -> {
-        }, schedule));
-        return scheduledTask;
     }
 }
